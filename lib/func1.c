269faa6ee98b7f85632be011992054ca913af9f5
Jeonga Han <ab012121@naver.com>
2018-10-16 00:36:43 -0700

    My initial commit

---
+++ lib/func1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"func1.h"
+
+void func1()
+{
+  printf("func1\n");
+
+}
---
+++ lib/func2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"func2.h"
+
+void func2()
+{
+  printf("func2\n");
+}
+
